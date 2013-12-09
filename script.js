5f9e120d1d238e3accc0d7e4b10a21de6c237a0a
Aaron Krajeski <aaronkrajeski@gmail.com>
2013-12-09 16:45:49 -0500

    Lotsa stuffz

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
 "use strict";
-var paper;
-var circle;
+var GRAVITY = 1; // A coefficient for our gravity
+// hint: Fg = G * m1 * m2 / r^2
+var DENSITY = 1; // Set a standard density for now.
+var TIME_STEP = 1; // One unit of time
 
-//An object storing the abstraction of our current screen 
-function ScreenGrid () {
+var paper;
+var planets = []; // An array containing all planets
+var circle;
+var testCirc;
+
+function update_display() 
+{
+	for (var i = 0; i < planets.length; i++) {
+		var nextPosition = planets[i].data('nextPosition');
+		planets[i].attr({
+			'cx': nextPosition[0],
+			'cy': nextPosition[1]
+		});
+	};
+}
+
+function find_gravitational_force(planet)
+{
+
+}
+
+// Mass is a scalar, position, velocity and gravitaional_force are two dimensional vectors
+function calculate_next_position(mass, position, velocity, gavitational_force)
+{
+	var m = mass;
+	var v0 = velocity;
+	var p0 = position;
+	var Fg = gravitaional_force;
 	
+	var a = [F[0]/m, F[1]/m];
+	var v1 = [v0[0] + a[0], v0[1] + a[1]];
+
+	planets[i].planetData('velocity') = v1;
+}
+
+//An object storing a single planet
+function Planet(args) 
+{
+	var _self = this;
+	this.position = args.position;
+	this.velocity = args.velocity;
+	this.mass = args.mass;
+	this.radius = args.radius;
+
+	this.create = function() 
+	{
+		_self.raphael = paper.circle(
+			_self.position[0],
+			_self.position[1],
+			_self.radius);
+	}
 }
 
 $(document).ready(function(){
@@ -19,7 +69,27 @@ $(document).ready(function(){
 
 	handlers();
 
+	testCirc = new Planet({'position': [625,215], 'radius':100});
+
 });
+
+function create_planet(clickEvent)
+{
+	var x = clickEvent.offsetX;
+	var y = clickEvent.offsetY;
+
+	var new_planet = paper.circle(x, y, 0);
+	new_planet.animate({'r': 200}, 2000);
+
+
+	$(paper.canvas).on('mouseup.create_planet', function(e) {
+		console.log(e.offsetX - x);
+		console.log(clickEvent.timeStamp - e.timeStamp);
+		$(paper.canvas).off('.create_planet');
+	});
+
+	planets.push(new_planet);
+}
 
 function handlers() {
 
@@ -35,7 +105,8 @@ function handlers() {
 				$(this).text(feedback[i]);
 			});
 		},
-		click: function(e){
+		mousedown: function(e){
+			create_planet(e);
 			circle.attr({'cx': e.offsetX, 'cy': e.offsetY});
 		}
 	});
