caa83cfa185bec188c79609d33e4b19e2db0d281
Patrick Estabrook <patrick.m.estabrook@gmail.com>
2013-12-18 23:33:05 -0500

    First Commit, planet.js -- moved from util.js to solve dependency issue

---
+++ planet.js
@@ -0,0 +1,14 @@
+// A planet on the screen
+function Planet(circle, position, velocity, acceleration) {
+	this.circle = circle; // The raphael object to which it is referred
+	this.position = position || new Vector(0,0);
+	this.velocity = velocity || new Vector(0,0);
+	this.acceleration = acceleration || new Vector(0,0);
+	this.mass;
+	this.radius;
+
+	this.move = function() {
+		this.velocity.add( this.acceleration.scale(ANIMATOR.TIME_STEP) );
+		this.position.add( this.velocity.scale(ANIMATOR.TIME_STEP) );
+	}
+}
