7e3e9e61f7a8d7146fce3fa8ae054fcf017b48f0
Patrick Estabrook <patrick.m.estabrook@gmail.com>
2013-12-18 23:37:48 -0500

    Because I totally meant PLANIMATOR

--- planet.js
+++ planet.js
@@ -8,7 +8,7 @@ function Planet(circle, position, velocity, acceleration) {
 	this.radius;
 
 	this.move = function() {
-		this.velocity.add( this.acceleration.scale(ANIMATOR.TIME_STEP) );
-		this.position.add( this.velocity.scale(ANIMATOR.TIME_STEP) );
+		this.velocity.add( this.acceleration.scale(PLANEMATOR.TIME_STEP) );
+		this.position.add( this.velocity.scale(PLANEMATOR.TIME_STEP) );
 	}
 }
