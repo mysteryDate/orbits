4cfeab6a2501ad924e2a6a08af7d924176edcfbb
Patrick Estabrook <patrick.m.estabrook@gmail.com>
2013-12-19 00:50:53 -0500

    Use single 'options' parameter

--- planet.js
+++ planet.js
@@ -1,9 +1,9 @@
 // A planet on the screen
-function Planet(circle, position, velocity, acceleration) {
-	this.circle = circle; // The raphael object to which it is referred
-	this.position = position || new Vector(0,0);
-	this.velocity = velocity || new Vector(0,0);
-	this.acceleration = acceleration || new Vector(0,0);
+function Planet(options) {
+	this.circle       = options.circle; // The raphael object to which it is referred
+	this.position     = options.position     || new Vector(0,0);
+	this.velocity     = options.velocity     || new Vector(0,0);
+	this.acceleration = options.acceleration || new Vector(0,0);
 	this.mass;
 	this.radius;
 
