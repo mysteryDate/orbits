f6afed3d61ef90264795d608eb48611b763f6acf
Patrick Estabrook <patrick.m.estabrook@gmail.com>
2013-12-18 22:54:50 -0500

    Lotsa format changes, some refactors mostly for style points, gettin' to know this motherlovin' code

--- script.js
+++ script.js
@@ -2,198 +2,181 @@
 // --------------------
 // Variables
 // --------------------
-var GRAVITY = 10; 		// A coefficient for our gravity, hint: Fg = G * m1 * m2 / r^2
-var DENSITY = 100; 		// Set a standard density for now.
-var TIME_STEP = 0.03;	// One unit of time
-var MAX_SIZE = 100;		// Maximum raidus of a planet (pixels)
-var GROW_TIME = 10000;	// Time in ms for a planet to grow to full size
-var BOUNDARY = 50;		// Number of pixels of escape before planet is deleted
-var COLLISION_F = 0.99;	// Coefficient for energy maintained in collisions
+var GRAVITY     = 10,    // A coefficient for our gravity, hint: Fg = G * m1 * m2 / r^2
+	DENSITY     = 100,   // Set a standard density for now.
+	TIME_STEP   = 0.03,  // One unit of time
+	MAX_SIZE    = 100,   // Maximum raidus of a planet (pixels)
+	GROW_TIME   = 10000, // Time in ms for a planet to grow to full size
+	BOUNDARY    = 50,    // Number of pixels of escape before planet is deleted
+	COLLISION_F = 0.99,  // Coefficient for energy maintained in collisions
 // ---------------------
 
 // ---------------------
 // Options
 // ---------------------
-var solid_border = true;	// Do planets reflect off the border?
+	solid_border = true, // Do planets reflect off the border?
 // ---------------------
 
-var paper;				// The canvas
-var Planets = []; 		// An array containing all Planets
+	paper,               // The canvas
+	Planets      = [],   // An array containing all Planets
 
-var windowWidth = $(window).width();
-var windowHeight = $(window).height();
+	windowWidth  = $(window).width(),
+	windowHeight = $(window).height(),
 
-var looping = false;
+	looping      = false,
+	lastRequest;
+
 // var accelerationLines = false;
 
-function loop() 
-{
+function loop() {
 	looping = true;
 	update();
 	draw();
 	queue();
 }
 
-var lastRequest;
-function stopLoop()
-{
+function stopLoop(){
 	looping = false;
 	window.cancelAnimationFrame(lastRequest);
 }
 
-function queue() 
-{
-  	lastRequest = window.requestAnimationFrame(loop);
+function queue() {
+	lastRequest = window.requestAnimationFrame(loop);
 }
 
-function update()
-{
-	for (var i = 0; i < Planets.length; i++) {
-		Planets[i].move();
-	};
+function update(){
+	Planets.forEach(function(planet){
+		planet.move()
+	});
+
 	calculate_gravitational_forces();
 }
 
-function draw() 
-{
-	for (var i = 0; i < Planets.length; i++) {
-		var p = Planets[i].position;
+function draw() {
+	var p, i;
+
+	Planets.forEach(function(planet, i){
+		p = planet.position;
 		// Planet is deleted if it leaves the zone
-		if(p.x < 0 - BOUNDARY || p.x > windowWidth + BOUNDARY 
-				|| p.y < 0 - BOUNDARY || p.y > windowHeight + BOUNDARY) {
-			Planets[i].circle.remove();
+		if (   p.x < 0 - BOUNDARY 
+			|| p.x > windowWidth + BOUNDARY 
+			|| p.y < 0 - BOUNDARY 
+			|| p.y > windowHeight + BOUNDARY) {
+			planet.circle.remove();
 			Planets.splice(i,1);
 		}
+
 		// Move the planet
-		else {
-			Planets[i].circle.attr({
-				'cx': p.x,
-				'cy': p.y
-			});
-		}
+		else planet.circle.attr({'cx': p.x, 'cy': p.y});
+
 		// Planet reflects on borders with this option
-		if(solid_border) {
-			var r = Planets[i].radius;
-			if(p.x - r <= 0)
-				Planets[i].velocity.x *= -COLLISION_F;
-			if(p.x + r >= windowWidth)
-				Planets[i].velocity.x *= -COLLISION_F;
-			if(p.y - r <= 0)
-				Planets[i].velocity.y *= -COLLISION_F;
-			if(p.y + r >= windowHeight)
-				Planets[i].velocity.y *= -COLLISION_F;
+		if (solid_border) {
+			var r = planet.radius;
+			if(p.x - r <= 0) planet.velocity.x *= -COLLISION_F;
+			if(p.y - r <= 0) planet.velocity.y *= -COLLISION_F;
+			if(p.x + r >= windowWidth)  planet.velocity.x *= -COLLISION_F;
+			if(p.y + r >= windowHeight) planet.velocity.y *= -COLLISION_F;
 		}
-	};
+	});
 }
 
-
-function calculate_gravitational_forces()
-{
+function calculate_gravitational_forces(){
 	// A list of all gravitational force vectors for each planet
-	var gravityVectors = [];
-	//var $fb = $('#feedback tbody tr td');
+	var gravityVectors = [],
+		i,
+		m1, p1, r1,
+		m2, p2, r2,
+		d,  dMag,
+		F,  Fmag;
 
-	for (var i = 0; i < Planets.length; i++) {
-		gravityVectors[i] = new Vector;
-	};
+	Planets.forEach(function(){
+		gravityVectors.push(new Vector);
+	});
 
-	for (var i = 0; i < Planets.length; i++) {
-		var m1 = Planets[i].mass;
-		var p1 = Planets[i].position;
-		var r1 = Planets[i].radius;
+	Planets.forEach(function(planet1, i, _this){
+		m1 = planet1.mass;
+		p1 = planet1.position;
+		r1 = planet1.radius;
 
 		for (var j = i+1; j < Planets.length; j++) {
-			var m2 = Planets[j].mass;
-			var p2 = Planets[j].position;
-			var r2 = Planets[j].radius;
-			var d = Vector.add(p2, p1.scale(-1));
-			var dMag = d.getMagnitude();
-			// $fb.text(Math.round( dMag - (r1 + r2)) );
+			m2   = Planets[j].mass;
+			p2   = Planets[j].position;
+			r2   = Planets[j].radius;
+			d    = Vector.add(p2, p1.scale(-1));
+			dMag = d.getMagnitude();
+
 			if( dMag <= r1 + r2 ) {
-				collide(Planets[i], Planets[j], d);
-				// console.log(dMag);
+				collide(planet1, Planets[j], d);
 			}
 			else {
-				var Fmag = GRAVITY*m1*m2/Math.pow(dMag,2);
-				var F = Vector.fromAngle(d.getAngle(), Fmag);
+				Fmag = GRAVITY*m1*m2/Math.pow(dMag,2);
+				F = Vector.fromAngle(d.getAngle(), Fmag);
 				gravityVectors[i].add(F);
 				gravityVectors[j].add(F.scale(-1)); // It's Newton's third law, Patrick!
 			}
 		};
 
-		// var Fpath = paper.path('M'+p1.x+' '+p1.y+'L'+(p1.x+gravityVectors[i].x/10)+' '+(p1.y+gravityVectors[i].y/10));
-		// Fpath.attr('stroke', 'red');
-		Planets[i].acceleration = gravityVectors[i].scale(1/m1);	// F = ma, bitches
-	};
+		planet1.acceleration = gravityVectors[i].scale(1/m1);	// F = ma, bitches
+	});
 }
 
 // Arguments are the two colliding bodies and a vector of the distance between them
-function collide(b1, b2, d) 
-{
+function collide(b1, b2, d) {
 	if (!d ) d = Vector.add(p2, p1.scale(-1));
 
 	// Initial velocities
-	var u1 = b1.velocity;
-	var u2 = b2.velocity;
+	var u1 = b1.velocity,
+		u2 = b2.velocity,
+		E1 = 0.5 * (   b1.mass*Math.pow(b1.velocity.getMagnitude(),2) 
+			         + b2.mass*Math.pow(b2.velocity.getMagnitude(),2)),
+		E2,
 
-	var E1 = 0.5*( b1.mass*Math.pow(b1.velocity.getMagnitude(),2) +
-		b2.mass*Math.pow(b2.velocity.getMagnitude(),2) );
+	    // Component of initial velocity projected onto d
+		u1d = d.scale( Vector.dotProduct(u1,d) / (Math.pow(d.getMagnitude(),2)) ),
+		u2d = d.scale( Vector.dotProduct(u2,d) / (Math.pow(d.getMagnitude(),2)) ),
 
-	// Component of initial velocity projected onto d
-	var u1d = d.scale( Vector.dotProduct(u1,d) / (Math.pow(d.getMagnitude(),2)) );
-	var u2d = d.scale( Vector.dotProduct(u2,d) / (Math.pow(d.getMagnitude(),2)) );
+	    // Perpendicular components of initial velocities
+		u1n = Vector.add(b1.velocity, u1d.scale(-1)),
+		u2n = Vector.add(b2.velocity, u2d.scale(-1)),
 
-	// Perpendicular components of initial velocities
-	var u1n = Vector.add(b1.velocity, u1d.scale(-1));
-	var u2n = Vector.add(b2.velocity, u2d.scale(-1));
+	    // Components along d after collision, per conservation of energy and linear momentum
+		v1d = ( Vector.add( u1d.scale(b1.mass - b2.mass), u2d.scale(2*b2.mass) ) ).scale(COLLISION_F/(b1.mass + b2.mass)),
+		v2d = ( Vector.add( u2d.scale(b2.mass - b1.mass), u1d.scale(2*b1.mass) ) ).scale(COLLISION_F/(b1.mass + b2.mass)),
 
-	// Components along d after collision, per conservation of energy and linear momentum
-	var v1d = ( Vector.add( u1d.scale(b1.mass - b2.mass), u2d.scale(2*b2.mass) ) ).scale(COLLISION_F/(b1.mass + b2.mass));
-	var v2d = ( Vector.add( u2d.scale(b2.mass - b1.mass), u1d.scale(2*b1.mass) ) ).scale(COLLISION_F/(b1.mass + b2.mass));
-
-	// Velocities after collision 
-	var v1 = Vector.add(v1d, u1n);
-	var v2 = Vector.add(v2d, u2n);
+	    // Velocities after collision 
+		v1 = Vector.add(v1d, u1n),
+		v2 = Vector.add(v2d, u2n);
 
 	b1.velocity = v1;
 	b2.velocity = v2;
-
-	var E2 = 0.5*( b1.mass*Math.pow(b1.velocity.getMagnitude(),2) +
-		b2.mass*Math.pow(b2.velocity.getMagnitude(),2) );
-
-	// console.log(E2 - E1);
+	E2 = 0.5 * (   b1.mass*Math.pow(b1.velocity.getMagnitude(),2)
+		         + b2.mass*Math.pow(b2.velocity.getMagnitude(),2));
 }
 
-$(document).ready(function(){
-
-	paper = new Raphael('container', '100%', '100%'); 
-	handlers();
-});
-
-function create_planet(clickEvent)
-{
+function create_planet(clickEvent) {
 	// I don't think the offset works in Firefox
-	var x = clickEvent.offsetX;
-	var y = clickEvent.offsetY;
+	var x      = clickEvent.offsetX,
+		y      = clickEvent.offsetY,
+		vPath,
+		new_planet,
+		circle = paper.circle(x, y, 5);
 
-	var circle = paper.circle(x, y, 5);
 	circle.animate({'r': MAX_SIZE}, GROW_TIME);
 	
 	// Velocity path
-	var vPath = paper.path('M'+x+' '+y+'L'+x+' '+y);
+	vPath = paper.path('M' + x + ' ' + y + 
+		               'L' + x + ' ' + y);
 
-	var new_planet = new Planet(circle, new Vector(x,y));
+	new_planet = new Planet(circle, new Vector(x,y));
 
-	$(paper.canvas).on('mousemove.create_planet', function(e)
-	{
+	$(paper.canvas).on('mousemove.create_planet', function(e) {
 		vPath.attr('path', 'M'+x+' '+y+'L'+e.offsetX+' '+e.offsetY);
 	});
-	$(paper.canvas).on('mouseup.create_planet', function(e) 
-	{
+	$(paper.canvas).on('mouseup.create_planet', function(e) {
 		circle.stop();
 
-		var v = new Vector(e.offsetX - x, e.offsetY - y);
-		var r = circle.attr('r');
+		var v = new Vector(e.offsetX - x, e.offsetY - y),
+			r = circle.attr('r');
 
 		new_planet.velocity = v;
 		new_planet.acceleration = new Vector(0, 0);
@@ -206,47 +189,4 @@ function create_planet(clickEvent)
 	});
 
 	Planets.push(new_planet);
-}
-
-window.onresize = function(e) {
-	windowWidth = $(window).width();
-	windowHeight = $(window).height();
-}
-
-function handlers() {
-
-	$(paper.canvas).on({
-		mousemove: function(e){
-			var feedback = [
-				e.clientX,	e.clientY,
-				e.pageX,	e.pageY,
-				e.screenX,	e.screenY,
-				e.offsetX,	e.offsetY];
-
-			$('#feedbackTable td').each(function(i,e){
-				$(this).text(feedback[i]);
-			});
-		},
-		mousedown: function(e){
-			stopLoop();
-			create_planet(e);
-		}
-	});
-
-	$('body').on('keypress', function(e){
-		console.log(e.which);
-		if( e.which == 32 ) {  // Spacebar
-			if(looping == true)
-				stopLoop();
-			else
-				loop();
-		}
-		if( e.which == 99) { // Clear on 'c'
-			for (var i = 0; i < Planets.length; i++) {
-				Planets[i].circle.remove();
-			};
-			Planets = [];
-
-		}
-	});
 }
